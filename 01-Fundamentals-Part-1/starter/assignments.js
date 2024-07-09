1ebaa4763ef58a67a4921b9fa6f6b97684c6d4cf
fasai1470 <fasai1470@gmail.com>
2024-07-09 17:53:12 +0700

    Fix: Add code in Data Types section

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -6,3 +6,11 @@ let population = 66;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// ------------------- Data Types -------------------
+let isIsland = false;
+let language = "";
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
