43848005312ebb52d55fd0079ec09d511b0abcdc
fasai1470 <fasai1470@gmail.com>
2024-07-09 17:42:29 +0700

    Feat: Add assignments.js file in folder 01-Fundamentals-Part-1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+// ------------------- Values and Variables -------------------
+let country = "Thailand";
+let continent = "Asia";
+let population = 66;
+
+console.log(country);
+console.log(continent);
+console.log(population);
