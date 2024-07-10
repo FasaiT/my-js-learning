47d10e3796c45436717d68e9f4b5c8fb50a4bca4
fasai1470 <fasai1470@gmail.com>
2024-07-10 17:33:09 +0700

    Feat: Add assignments.js file in folder 02-Fundamentals-Part-2

---
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -0,0 +1,118 @@
+// ------------------- Functions -------------------
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} people and its capital city is ${capitalCity}`;
+}
+
+const describeThailand = describeCountry("Thailand", 66, "Bangkok");
+const describeGermany = describeCountry("Germany", 83, "Berlin");
+const describeFinland = describeCountry("Finland", 6, "Helsinki");
+
+console.log(describeThailand);
+console.log(describeGermany);
+console.log(describeFinland);
+
+// ------------------- Function Declarations vs. Expressions -------------------
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const poplThailand1 = percentageOfWorld1(66);
+const poplGermany1 = percentageOfWorld1(83);
+const poplFinland1 = percentageOfWorld1(6);
+
+const poplThailand2 = percentageOfWorld2(66);
+const poplGermany2 = percentageOfWorld2(83);
+const poplFinland2 = percentageOfWorld2(6);
+
+console.log(poplThailand1, poplGermany1, poplFinland1);
+console.log(poplThailand2, poplGermany2, poplFinland2);
+
+// ------------------- Arrow Functions -------------------
+//ref from w3 school
+const percentageOfWorld3 = (population) => {
+  return (population / 7900) * 100;
+};
+const poplThailand3 = percentageOfWorld3(66);
+const poplGermany3 = percentageOfWorld3(83);
+const poplFinland3 = percentageOfWorld3(6);
+
+console.log(poplThailand3, poplGermany3, poplFinland3);
+
+// KEY
+// const percentageOfWorld3 = population => (population / 7900) * 100;
+// const percPortugal3 = percentageOfWorld3(10);
+// const percChina3 = percentageOfWorld3(1441);
+// const percUSA3 = percentageOfWorld3(332);
+
+// console.log(percPortugal3, percChina3, percUSA3);
+
+// ------------------- Functions Calling Other Functions -------------------
+function describePopulation(country, population) {
+  return `${country} has ${population} million people, which is about ${percentageOfWorld1(
+    population
+  )}% of the world`;
+}
+const descPoplThailand = describePopulation("Thailand", 66);
+const descPoplGermany = describePopulation("Germany", 83);
+const descPoplFinland = describePopulation("Finland", 6);
+// const descPoplChina = describePopulation("China", 1441);
+
+console.log(descPoplThailand);
+console.log(descPoplGermany);
+console.log(descPoplFinland);
+// console.log(descPoplChina);
+
+// ------------------- Introduction to Arrays -------------------
+let populations = [66, 10, 1441, 332];
+console.log(populations.length === 4);
+
+let percentages = [
+  percentageOfWorld1(populations[0]),
+  percentageOfWorld1(populations[1]),
+  percentageOfWorld1(populations[2]),
+  percentageOfWorld1(populations[3]),
+];
+
+console.log(percentages);
+
+// ------------------- ***Basic Array Operations (Methods)*** -------------------
+let neighbours = ["Myanmar", "Laos", "Malaysia"];
+neighbours.push("Utopia");
+neighbours.pop("Utopia");
+console.log(neighbours);
+
+if (!neighbours.includes("Germany")) {
+  console.log(`Probably not a central european country :D`);
+}
+
+neighbours[neighbours.indexOf("Laos")] = `Lao People's Democratic Republic`;
+console.log(neighbours);
+
+// ------------------- ***Introduction to Objects*** -------------------
+const myCountry = {
+  country: "Thailand",
+  capital: "Bangkok",
+  language: "Thai",
+  population: 66,
+  neighbours: ["Myanmar", "Laos", "Malaysia"],
+};
+console.log(myCountry);
+
+// ------------------- ***Dot vs. Bracket Notation*** -------------------
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+);
+
+myCountry.population += 2;
+console.log(myCountry.population);
+
+myCountry["population"] -= 2;
+console.log(myCountry.population);
+
+//รูปแบบของการใช้งาน . dot notation & [ ] Bracket Notation แตกต่างกันตรงที่
+//dot notation จะไม่สามารถเอาชื่อตัวแปรมาอ้างอิง เพื่อเข้าถึงvalue ข้างในโดยตรงได้ ถ้าจะเข้าถึง ต้องเอา key ที่มีอยู่เท่านั้น ถึงจะได้ valueข้างในออกมา
+//Bracket Notation เหมือนเป็นการเข้าถึง index ของแอรเรย์ตัวนั้น ๆ โดยตรง โดยที่ค่าในรูปแบบนี้ เอาชื่อตัวแปรมาใช้ได้ เอาอะไรมาใช้ก็แต่ต้องอยู่ในรูปแบบของสตริงเท่านั้น ถึงจะเข้าถึง value ข้างในได้
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -451,31 +451,31 @@ console.log(age1, age2); */
 // console.log(bills, tips, totals);
 
 // KEYS
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
 
-/* Write your code below. Good luck! 🙂 */
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+// /* Write your code below. Good luck! 🙂 */
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
 
-for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[1]);
-  tips.push(tip);
-  totals.push(tip + bills[i]);
-}
+// for (let i = 0; i < bills.length; i++) {
+//   const tip = calcTip(bills[1]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
 
-console.log(bills, tips, totals);
+// console.log(bills, tips, totals);
 
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    // sum = sum + arr [i];
-    sum += arr[i];
-  }
-  return sum / arr.length;
-}
-console.log(calcAverage([2,3,7]));
-console.log(calcAverage(totals));
-console.log(calcAverage(tips));
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     // sum = sum + arr [i];
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// }
+// console.log(calcAverage([2,3,7]));
+// console.log(calcAverage(totals));
+// console.log(calcAverage(tips));
